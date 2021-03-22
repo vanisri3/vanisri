bc2082237dc353c1ade8222930714ee0ff52927c
Asus <Asus@LAPTOP-8L3GFD5B>
2021-03-22 22:59:32 +0800

    new

---
+++ Test1/src/exercisess.java
@@ -0,0 +1,16 @@
+
+public class exercisess {
+
+	public static void main(String[] args) {
+		int array [] = { 1,2,3,4};
+		int sum = 0;
+		for ( int i = 0; i < array.length; i ++) {
+			sum+=array[i];
+			
+		}
+		System.out.println ( " sum of array is " + sum);
+
+
+	}
+
+}
